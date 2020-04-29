fd29240040993c5dd40df0e8974c18238890d1b8
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-29 23:25:25 +0530

    ADDED: test to catch exception when wrong file given

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -40,6 +40,16 @@ public class CensusAnalyserTest {
         } catch (CensusAnalyserException e) { }
     }
 
-
+    @Test
+    public void givenIndiaStateCodeData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaStateCodeData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
 
 }
